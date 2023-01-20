47090cb3b255857b9494a890c7a7778bf9a0b84c
shohann <shohanurr490@gmail.com>
2023-01-20 20:24:02 +0600

    new feature

---
+++ src/middlewares/addView.js
@@ -0,0 +1,4 @@
+
+module.exports.addView = async (req, res, next) => {
+    
+}
---
+++ src/models/view.js
@@ -0,0 +1,15 @@
+const { Schema, model } = require('mongoose');
+
+// module.exports.View = model(
+//     'View', 
+//     Schema({
+//         count: {
+//             type: number,
+//             requred: true,
+//         },
+//         news: {
+//             type: Schema.Types.ObjectId,
+//             ref: 'News'
+//         }
+//     })
+// );
