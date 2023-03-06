d2b5f4ec9b2e4068e1fe2319dba5c12782dd8c46
shohann <shohanurr490@gmail.com>
2023-03-06 20:36:56 +0600

    comment section fixed

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -10,15 +10,15 @@ module.exports.setComment = async (req, res, next) => {
         const commentText = req.body.commentText;
         const userId = req.user.id; 
 
-        await runInTransaction(async (session) => {
-            const { _id } = await createComment({
-                commentText: commentText,
-                news: newsId,
-                user: userId
-            }, session);
-            await updateNewsCommentsById(_id, newsId, session);
-            await updateUsersCommentsById(_id, userId, session);
-        });
+        // await runInTransaction(async (session) => {
+        //     const { _id } = await createComment({
+        //         commentText: commentText,
+        //         news: newsId,
+        //         user: userId
+        //     }, session);
+        //     await updateNewsCommentsById(_id, newsId, session);
+        //     await updateUsersCommentsById(_id, userId, session);
+        // });
 
         res.status(201).json({ 
             success: true,
